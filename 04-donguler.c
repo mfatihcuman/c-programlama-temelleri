2f3fffd582fbeb072e1b03cce4073a24723fdcb2
mfatihcuman <muhammedfati1661@gmail.com>
2025-07-15 14:03:49 +0300

    Update 04-hızlı değer atmama.c

---
+++ 04-donguler.c
@@ -0,0 +1,25 @@
+/* BTK Akademi -C Programlama Dili Kursu*/
+/**
+ *  C programlama dilinde hızlı atama özelliği
+ * */
+ 
+ #include <stdio.h>
+  int main()
+  {
+    int i = 6;
+    printf("%d\n",i);
+    
+    i += 5;  //i = i + 5
+    printf("%d\n",i);
+    
+    i -= 5;  //i = i - 5
+    printf("%d\n",i);
+    
+    i *= 7;  //i = i * 7
+    printf("%d\n",i);
+    
+    i /= 5;  //i = i / 5
+    printf("%d\n",i);
+    
+    return 0;
+  }
